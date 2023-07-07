ccdf4970d9bf5c67ad7be162745595ea7715f157
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-07 14:00:36 -0500

    added a addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
 console.log('***** Music Collection *****')
+
+//an empty array that is called collection
+let collection=[];
+
+function addToCollection(title,artist,yearPublished){
+    //create an object having the above properties
+    console.log('I am inside the function');
+    let collectionObject ={
+        titleObject:title,
+        artistObject:artist,
+        yearPublishedObject:yearPublished
+    }//end collectionObject
+    //Add the new object to the end of the collection array
+    collection.push(collectionObject);
+    //Return the newly created object
+    return collection;
+}//end addToCollection
+
+//test the function
+console.log('My collection is:', collection);
+console.log('Adding to my collection', addToCollection('Asiya','Mohamed',2019));
+console.log('My collection is now:', collection);
