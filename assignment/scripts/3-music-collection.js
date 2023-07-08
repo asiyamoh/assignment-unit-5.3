48a62e590249aa133357eccb9d35ecfe5d7846a6
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-07 21:23:59 -0500

    made changes to findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,12 +18,12 @@ function addToCollection(title,artist,yearPublished){
 
 //test the function
 console.log('My collection is:', collection);
-console.log('Adding to my collection', addToCollection('Phone Numbers','Dominic Fike',2019));
-console.log('Adding to my collection', addToCollection('3 Nights','Dominic Fike',2018));
-console.log('Adding to my collection', addToCollection('Babydoll','Dominic Fike',2018));
-console.log('Adding to my collection', addToCollection('Chicken Tendies','Clinton Kane',2022));
-console.log('Adding to my collection', addToCollection('Fix it to break it','Clinton Kane',2020));
-console.log('Adding to my collection', addToCollection('Avo Toast','Clinton Kane',2022));
+addToCollection('Phone Numbers','Dominic Fike',2019);
+addToCollection('3 Nights','Dominic Fike',2018);
+addToCollection('Babydoll','Dominic Fike',2018);
+addToCollection('Chicken Tendies','Clinton Kane',2022);
+addToCollection('Fix it to break it','Clinton Kane',2020);
+addToCollection('Avo Toast','Clinton Kane',2022);
 console.log('My collection is now:', collection);
 
 
@@ -37,9 +37,27 @@ function showCollection(array){
         + array[i].yearPublished;
         console.log(result);
     }//end for loop
-    return true
 }//end showCollection
 
 //test the function 
-console.log('It should say true: ', showCollection(collection));
+showCollection(collection);
 
+function findByArtist(artist){
+    console.log('inside  the function');
+    //Create an array to hold any results, empty to start
+    let array=[];
+        //add any objects with a matching artist to the array.
+        if(array.artist == artist){
+            console.log('inside the if loop');
+            array += artist;
+        }//end match
+        //if no results are found, return an empty array.
+        else{
+            console.log('inside the else loop');
+            array=[];
+        }//end not a match
+    return array;
+}//end findByArtist
+
+//test the function
+console.log(findByArtist('Dominic Fike'));
